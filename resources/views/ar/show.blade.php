f2677f703847b92610712b14be06965d2d3f8b87
ricomuh <rico.smartlolo@gmail.com>
2023-09-08 10:04:32 +0700

    remove head

--- resources/views/ar/show.blade.php
+++ resources/views/ar/show.blade.php
@@ -1,37 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<title>
+    {{ $product->name }}
+</title>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        {{ $product->name }}
-    </title>
+<script src="{{ asset('js/aframe-master.min.js') }}"></script>
+<script src="{{ asset('js/aframe-ar-nft.js') }}"></script>
 
-    <script src="{{ asset('js/aframe-master.min.js') }}"></script>
-    <script src="{{ asset('js/aframe-ar-nft.js') }}"></script>
+<style>
+    .arjs-loader {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.8);
+        z-index: 9999;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-    <style>
-        .arjs-loader {
-            height: 100%;
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            background-color: rgba(0, 0, 0, 0.8);
-            z-index: 9999;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .arjs-loader div {
-            text-align: center;
-            font-size: 1.25em;
-            color: white;
-        }
-    </style>
-</head>
+    .arjs-loader div {
+        text-align: center;
+        font-size: 1.25em;
+        color: white;
+    }
+</style>
 
 <body>
     <div class="arjs-loader">
