6a283b6f1cffa4525b249175c812f4e027634d1e
ricomuh <rico.smartlolo@gmail.com>
2023-09-09 19:28:53 +0700

    interactable

--- resources/views/ar/show.blade.php
+++ resources/views/ar/show.blade.php
@@ -50,12 +50,12 @@
     </div>
     <a-scene vr-mode-ui="enabled: false;" renderer="logarithmicDepthBuffer: true; precision: medium;" embedded arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
         <a-nft type="nft" url="{{ 'storage/' . $product->marker }}" smooth="true" smoothCount="10" smoothTolerance=".01" smoothThreshold="5" emitevents="true" id="nft">
-            <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="5 5 5" position="0 0 0" rotation="0 180 0"></a-entity>
+            <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="5 5 5" position="150 300 -100" rotation="0 180 0" id="model"></a-entity>
             <a-text value="{{ $product->description }}" position="0 -50 0" align="center" width="120" color="#000000"></a-text>
         </a-nft>
         <a-entity camera></a-entity>
     </a-scene>
-
+    <script src="https://hammerjs.github.io/dist/hammer.min.js"></script>
     <script>
         const markerIndicator = document.querySelector('#marker-indicator');
         const audio = new Audio('{{ asset("storage/" . $product->music) }}');
@@ -79,6 +79,46 @@
         marker.addEventListener('markerLost', (ev) => {
             audio.pause();
             setMarkerIndicatorColor('red');
+        });
+
+        const model = document.querySelector('#model');
+        const mc = new Hammer.Manager(model);
+
+        // add pinch and pan recognizers to the Hammer manager
+        mc.add(new Hammer.Pinch());
+        mc.add(new Hammer.Pan());
+
+        // set initial scale and position values
+        let scale = 1;
+        let posX = 0;
+        let posY = 0;
+
+        // add event listeners for pinch and pan events
+        mc.on('pinch', (ev) => {
+            // update the scale value based on the pinch event
+            scale *= ev.scale;
+            model.setAttribute('scale', `${scale} ${scale} ${scale}`);
+        });
+
+        mc.on('pan', (ev) => {
+            let rotation = model.getAttribute("rotation")
+            switch (ev.direction) {
+                case 2:
+                    rotation.y = rotation.y + 4
+                    break;
+                case 4:
+                    rotation.y = rotation.y - 4
+                    break;
+                case 8:
+                    rotation.x = rotation.x + 4
+                    break;
+                case 16:
+                    rotation.x = rotation.x - 4
+                    break;
+                default:
+                    break;
+            }
+            model.setAttribute("rotation", rotation)
         });
     </script>
 </body>
