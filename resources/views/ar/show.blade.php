dabda5aa2b7b48c69e62f6ec4dd32011b1b14da2
ricomuh <rico.smartlolo@gmail.com>
2023-09-20 23:11:44 +0700

    rollback but with animation and link

--- resources/views/ar/show.blade.php
+++ resources/views/ar/show.blade.php
@@ -10,8 +10,6 @@
     <script src="{{ asset('js/aframe-master.min.js') }}"></script>
     <script src="{{ asset('js/aframe-ar-nft.js') }}"></script>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <style>
         .arjs-loader {
             height: 100%;
@@ -32,6 +30,11 @@
             color: white;
         }
 
+        body {
+            margin: 0;
+            overflow: hidden;
+        }
+
         #marker-indicator {
             position: absolute;
             top: 10px;
@@ -49,32 +52,27 @@
 
     <div id="marker-indicator"></div>
     <!-- back button -->
-    <a href="{{ route('home') }}" style="position: absolute; top: 10px; left: 10px; z-index: 9999; font-size: 1.5em; color: white; text-decoration: none;" class="absolute top-2 left-2 text-base text-white hover:text-gray-200 flex items-center gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-        </svg>
-        <span>Back</span>
-    </a>
+    <a href="{{ route('home') }}" style="position: absolute; top: 10px; left: 10px; z-index: 9999; font-size: 1.5em; color: white; text-decoration: none;">&larr; Back</a>
     <!-- product description show when the marker found -->
-    <div id="description" style="display: none;" class="fixed bottom-2 w-11/12 px-4 py-2 mx-auto text-sm text-center text-white bg-gray-900 rounded-md flex flex-col gap-2">
+    <div id="description" style="position: fixed; bottom: 10px; left: 50%; transform: translateX(-50%); z-index: 9999; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 10px; display: none; width: 80%; text-align: center;">
         <p>{{ $product->description }}</p>
         @if ($product->link)
-        <a href="{{ $product->link }}" target="_blank" class="px-4 py-2 text-sm text-center text-white bg-blue-500 rounded-md flex justify-center">
+        <a href="{{ $product->link }}" target="_blank" style="color: white; text-decoration: none;">
             Learn More
         </a>
-        @endif
     </div>
 
     <div class="arjs-loader">
         <div>Loading, please wait...</div>
     </div>
-    <a-scene vr-mode-ui="enabled: false;" renderer="logarithmicDepthBuffer: true; precision: medium;" embedded arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
-        <a-nft type="nft" url="{{ 'storage/' . $product->marker }}" smooth="true" smoothCount="10" smoothTolerance=".01" smoothThreshold="5" emitevents="true" id="nft">
-            <!-- rotate animation -->
-            <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="{{ $product->scale }}" position="{{ $product->position }}" rotation="{{ $product->rotation }}" id="model" animation="property: rotation; to: {{ $product->model_rotation_x }} {{ $product->model_rotation_y + 360 }} {{ $product->model_rotation_z }}; loop: true; dur: 10000; easing: linear"></a-entity>
-        </a-nft>
-        <a-entity camera></a-entity>
-    </a-scene>
+    <div style="width: 100%; height: 100%;">
+        <a-scene vr-mode-ui="enabled: false;" renderer="logarithmicDepthBuffer: true; precision: medium;" embedded arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
+            <a-nft type="nft" url="{{ 'storage/' . $product->marker }}" smooth="true" smoothCount="10" smoothTolerance=".01" smoothThreshold="5" emitevents="true" id="nft">
+                <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="{{ $product->scale }}" position="{{ $product->position }}" rotation="{{ $product->rotation }}" id="model" animation="property: rotation; to: {{ $product->model_rotation_x }} {{ $product->model_rotation_y + 360 }} {{ $product->model_rotation_z }}; loop: true; dur: 10000; easing: linear"></a-entity>
+            </a-nft>
+            <a-entity camera></a-entity>
+        </a-scene>
+    </div>
 
     <script>
         const markerIndicator = document.querySelector('#marker-indicator');
