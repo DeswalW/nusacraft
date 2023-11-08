d370a551dd36218c8643b130dc89e6d531dbb122
farisfaikar <farisfaikar.r@gmail.com>
2023-11-08 17:40:16 +0700

    fix: detail layouts and view

--- resources/views/ar/detail.blade.php
+++ resources/views/ar/detail.blade.php
@@ -1,24 +1,15 @@
 <x-home-layout>
     <div class="flex flex-col items-center justify-center py-8 gap-10">
-        <h2 class="text-2xl font-bold">Our Products</h2>
-        <!-- product list -->
-        <div class="grid grid-cols-4 gap-2">
-            <div class="flex justify-center align-center col-span-2">
-                <img src="{{ asset('storage/' . $product->image) }}" alt="">
+        <div class="flex flex-col gap-5 p-8">
+            <h2 class="text-2xl font-bold self-center">
+                {{ $product->name }}
+            </h2>
+            <div class="max-h-64 flex justify-center">
+                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
             </div>
-            <div class="flex flex-col gap-4 col-span-2">
-                <h3 class="text-xl font-bold">
-                    {{ $product->name }}
-                </h3>
-                <p>
-                    {{ $product->description }}
-                </p>
-            </div>
-            <div class="col-span-4">
-                <p>
-                    {{ $product->description }}
-                </p>
-            </div>
+            <p>
+                {{ $product->description }}
+            </p>
         </div>
     </div>
 </x-home-layout>
