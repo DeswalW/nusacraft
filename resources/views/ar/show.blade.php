55f3352332e79c8434a29fa1a910d7e4de925334
ricomuh <rico.smartlolo@gmail.com>
2023-11-09 22:39:09 +0700

    fix: more rollback

--- resources/views/ar/show.blade.php
+++ resources/views/ar/show.blade.php
@@ -9,7 +9,6 @@
 
     <script src="{{ asset('js/aframe-master.min.js') }}"></script>
     <script src="{{ asset('js/aframe-ar-nft.js') }}"></script>
-    <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
 
     <style>
         .arjs-loader {
@@ -72,24 +71,11 @@
             <a-nft type="nft" url="{{ 'storage/' . $product->marker }}" smooth="true" smoothCount="10" smoothTolerance=".01" smoothThreshold="5" emitevents="true" id="nft">
                 <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="{{ $product->scale }}" position="{{ $product->position }}" rotation="{{ $product->rotation }}" id="model" animation="property: rotation; to: {{ $product->model_rotation_x }} {{ $product->model_rotation_y + 360 }} {{ $product->model_rotation_z }}; loop: true; dur: 10000; easing: linear" material="shader: flat;"></a-entity>
             </a-nft>
-            <a-entity camera id="camera">
-            </a-entity>
+            <a-entity camera></a-entity>
         </a-scene>
     </div>
 
     <script>
-        // let location = null;
-        // AFRAME.registerComponent('locationfinder', {
-        //     init() {
-        //         console.log("init");
-        //         navigator.geolocation.getCurrentPosition((position) => {
-        //             console.log("posisi: ", position);
-        //             location = position;
-        //         });
-        //     }
-        // }
-        // );
-
         // set to full screen and landscape mode
         const aScene = document.querySelector('a-scene');
         if (aScene.requestFullscreen) {
@@ -103,38 +89,6 @@
         const audio = new Audio('{{ asset("storage/" . $product->music) }}');
         const marker = document.querySelector('#nft');
         const description = document.querySelector('#description');
-        const camera = document.querySelector('#camera');
-
-        // const removeEntity = () => {
-        //     const model = document.querySelector('#modelLocationBased');
-        //     model.parentNode.removeChild(model);
-        // }
-
-        // const createEntity = () => {
-        //     // if the model is exist, remove it
-        //     if (document.querySelector('#modelLocationBased')) {
-        //         removeEntity();
-        //     }
-
-        //     // create new model
-        //     // const model = document.createElement('a-entity');
-        //     // model.setAttribute('gltf-model', '{{ "/storage/" . $product->model }}');
-        //     // model.setAttribute('scale', '{{ $product->scale }}');
-        //     // model.setAttribute('position', '{{ $product->position }}');
-        //     // model.setAttribute('rotation', '{{ $product->rotation }}');
-        //     // model.setAttribute('id', 'modelLocationBased');
-        //     // model.setAttribute('animation', `property: rotation; to: {{ $product->model_rotation_x }} {{ $product->model_rotation_y + 360 }} {{ $product->model_rotation_z }}; loop: true; dur: 10000; easing: linear`);
-        //     // model.setAttribute('material', 'shader: flat;');
-        //     // model.setAttribute('gps-entity-place', `latitude: ${location.coords.latitude}; longitude: ${location.coords.longitude};`);
-        //     // model.setAttribute('gps-entity-place-added', true);
-        //     // model.setAttribute('look-at', '[gps-camera]');
-
-        //     // return model;
-
-        //     return `
-        //         <a-entity gltf-model="{{ '/storage/' . $product->model }}" scale="{{ $product->scale }}" position="{{ $product->position }}" rotation="{{ $product->rotation }}" id="model" animation="property: rotation; to: {{ $product->model_rotation_x }} {{ $product->model_rotation_y + 360 }} {{ $product->model_rotation_z }}; loop: true; dur: 10000; easing: linear" material="shader: flat;" gps-entity-place="latitude: ${location.coords.latitude}; longitude: ${location.coords.longitude};" gps-entity-place-added="true" look-at="[gps-camera]"></a-entity>
-        //     `;
-        // }
 
 
         const setMarkerIndicatorColor = (color) => {
@@ -150,14 +104,6 @@
             audio.play();
             setMarkerIndicatorColor('green');
             description.style.display = 'block';
-            // camera.removeAttribute('gps-camera');
-            // camera.removeAttribute('rotation-reader');
-            // camera.removeAttribute('look-controls');
-
-
-            // removeEntity();
-
-            // marker.insertAdjacentHTML('beforeend', createEntity());
         });
 
         // when the camera is not looking at the marker, pause music from $product->music
@@ -165,13 +111,6 @@
             audio.pause();
             setMarkerIndicatorColor('red');
             description.style.display = 'none';
-            // camera.setAttribute('gps-camera', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`);
-            // camera.setAttribute('rotation-reader', '');
-            // camera.setAttribute('look-controls', '');
-
-            // aScene.insertAdjacentHTML('beforeend', createEntity());
-
-            // marker.insertAdjacentHTML('beforeend', createEntity());
         });
     </script>
 </body>
