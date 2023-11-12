984ba070ccc86fcefd3b9087c07d92860fd6ff27
DeswalW <127175363+DeswalW@users.noreply.github.com>
2023-11-12 12:43:04 +0700

    Update detail.blade.php

--- resources/views/ar/detail.blade.php
+++ resources/views/ar/detail.blade.php
@@ -1,27 +1,31 @@
 
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>
-        {{ $product->name }}
-    </title>
-</head>
-<body>
-    <!-- back button -->
-    <a href="{{ route('home') }}" style="position: absolute; top: 10px; left: 10px; z-index: 9999; font-size: 1.5em; color: rgb(0, 0, 0); text-decoration: none;">&larr; Back</a>
-    <div>
-        <h2 class="text-2xl font-bold flex justify-center">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>
             {{ $product->name }}
-        </h2>
-    
-        <div class="max-h-full flex justify-center">
-            <img src="{{ asset('storage/' . $product->image_description) }}" alt="{{ $product->name }}">
+        </title>
+    </head>
+    <body>
+        <div class="mx-auto max-w-md sm:bg-red-200 md:bg-green-200 lg:bg-yellow-200 sm:max-w-xl md:max-w-2xl lg:max-w-3xl">
+            <!-- back button -->
+            <a href="#" style="position: absolute; top: 10px; left: 10px; z-index: 9999; font-size: 1.5em; color: rgb(0, 0, 0); text-decoration: none;">&larr; Back</a>
+            <!-- content -->
+            <div class="mt-12 px-6">
+                <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold flex justify-center">
+                    {{ $product->name }}
+                </h2>
+                <div class="max-w-full flex mt-4">
+                    <img src="{{ asset('storage/' . $product->image_description) }}" alt="{{ $product->name }}" class="rounded-xl sm:object-cover sm:h-auto sm:w-full md:object-cover lg:object-cover justify-center">
+                </div>
+                <p class="text-justify mt-4">
+                    {!! nl2br($product->detail) !!}
+                </p>
+            </div>
         </div>
-        <p class="text-justify">
-            {{ $product->detail }}
-        </p>
-    </div>
+    </body>
+    </html>
 </body>
 </html>
